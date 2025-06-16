a1e4c163e52eeba143dce752f141e6ae18346a50
Gilson Nunes <GilsonNunes_dev@outlook.com>
2025-06-16 01:38:07 +0000

    feat(validation): implement global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import "reflect-metadata";
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module.js";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(
+		new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+	);
 	await app.listen(3000);
 }
 bootstrap();
