974edeb83483d102107bcad738379dc8cfb8f751
Gilson Nunes <GilsonNunes_dev@outlook.com>
2025-06-16 23:56:57 -0300

    chore(server): add env variable for server port

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module.js";
 
 async function bootstrap() {
+	const PORT = process.env.PORT || 3000;
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(
 		new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
 	);
-	await app.listen(3000);
+	await app.listen(PORT);
 }
 bootstrap();
